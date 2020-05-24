87621bedd0f5939728c188fd910e6b6fc89a3be0
Your Name <you@example.com>
2020-05-24 18:11:33 +0530

    first commit

---
+++ Loanreport/Form2.Designer.vb
@@ -0,0 +1,176 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.dataview1 = New System.Windows.Forms.DataGridView()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.LoanledgerDataSet = New Loanreport.LoanledgerDataSet()
+        Me.LoanBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.LoanTableAdapter = New Loanreport.LoanledgerDataSetTableAdapters.LoanTableAdapter()
+        CType(Me.dataview1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.LoanledgerDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.LoanBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dataview1
+        '
+        Me.dataview1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dataview1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.dataview1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dataview1.Location = New System.Drawing.Point(2, 180)
+        Me.dataview1.Name = "dataview1"
+        Me.dataview1.RowTemplate.Height = 24
+        Me.dataview1.Size = New System.Drawing.Size(1086, 436)
+        Me.dataview1.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(41, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(130, 32)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Search"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(45, 59)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(126, 34)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Print"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.DataSource = Me.LoanBindingSource
+        Me.ComboBox1.DisplayMember = "LoanNo"
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(117, 31)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(226, 24)
+        Me.ComboBox1.TabIndex = 4
+        Me.ComboBox1.ValueMember = "LoanNo"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 31)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(93, 25)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Loan No"
+        '
+        'Panel1
+        '
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.ComboBox1)
+        Me.Panel1.Location = New System.Drawing.Point(11, 21)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(407, 100)
+        Me.Panel1.TabIndex = 6
+        '
+        'Panel2
+        '
+        Me.Panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel2.Controls.Add(Me.Button1)
+        Me.Panel2.Controls.Add(Me.Button2)
+        Me.Panel2.Location = New System.Drawing.Point(435, 21)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(200, 100)
+        Me.Panel2.TabIndex = 7
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Panel1)
+        Me.GroupBox1.Controls.Add(Me.Panel2)
+        Me.GroupBox1.Location = New System.Drawing.Point(109, 31)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(686, 143)
+        Me.GroupBox1.TabIndex = 6
+        Me.GroupBox1.TabStop = False
+        '
+        'LoanledgerDataSet
+        '
+        Me.LoanledgerDataSet.DataSetName = "LoanledgerDataSet"
+        Me.LoanledgerDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'LoanBindingSource
+        '
+        Me.LoanBindingSource.DataMember = "Loan"
+        Me.LoanBindingSource.DataSource = Me.LoanledgerDataSet
+        '
+        'LoanTableAdapter
+        '
+        Me.LoanTableAdapter.ClearBeforeFill = True
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1091, 628)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.dataview1)
+        Me.Name = "Form2"
+        Me.Text = "Details"
+        CType(Me.dataview1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        CType(Me.LoanledgerDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.LoanBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents dataview1 As System.Windows.Forms.DataGridView
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents Panel2 As System.Windows.Forms.Panel
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents LoanledgerDataSet As Loanreport.LoanledgerDataSet
+    Friend WithEvents LoanBindingSource As System.Windows.Forms.BindingSource
+    Friend WithEvents LoanTableAdapter As Loanreport.LoanledgerDataSetTableAdapters.LoanTableAdapter
+End Class
---
+++ Loanreport/Form2.vb
@@ -0,0 +1,77 @@
+﻿Imports System.Data.OleDb
+Imports CrystalDecisions.CrystalReports.Engine
+Imports CrystalDecisions.Shared
+
+
+Public Class Form2
+    Private conn As New OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=G:\interview project\Loan Ledger\Loanreport\Loanreport\bin\Debug\Loanledger.accdb")
+    Private Sub Form2_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        'TODO: This line of code loads data into the 'LoanledgerDataSet.Loan' table. You can move, or remove it, as needed.
+        Me.LoanTableAdapter.Fill(Me.LoanledgerDataSet.Loan)
+        'TODO: This line of code loads data into the 'LoanledgerDataSet1.Loan' table. You can move, or remove it, as needed.
+
+        'TODO: This line of code loads data into the 'LoanledgerDataSet.Loan' table. You can move, or remove it, as needed.
+        Me.LoanTableAdapter.Fill(Me.LoanledgerDataSet.Loan)
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        Dim search As String = "Select * from Loan where LoanNo='" & ComboBox1.Text & "'"
+        conn.Open()
+        Dim cmd As New OleDbCommand(search, conn)
+        Dim adpt As New OleDbDataAdapter(cmd)
+        Dim table As New DataTable
+        Dim ds As New DataSet
+        ds.Tables.Add(table)
+        adpt.Fill(table)
+        If table.Rows.Count <> 0 Then
+
+
+            dataview1.DataSource = table.DefaultView
+            conn.Close()
+        Else
+            MsgBox("Data not Found", MsgBoxStyle.Information)
+        End If
+
+
+
+
+        '  Dim reportdocument As CrystalDecisions.CrystalReports.Engine.ReportDocument
+        ' reportdocument = New CrystalReport1
+        'reportdocument.SetDataSource(table)
+        'Form3.CrystalReportViewer1.ReportSource = reportdocument
+        'Form3.ShowDialog()
+        'Form3.Dispose()
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+
+
+        Dim search As String = "Select * from Loan where LoanNo='" & ComboBox1.Text & "'"
+        conn.Open()
+        Dim cmd As New OleDbCommand(search, conn)
+        Dim adpt As New OleDbDataAdapter(cmd)
+        Dim table As New DataTable
+        Dim ds As New DataSet
+        ds.Tables.Add(table)
+        adpt.Fill(table)
+        'dataview1.DataSource = table.DefaultView
+        conn.Close()
+
+
+
+        Dim reportdocument As CrystalDecisions.CrystalReports.Engine.ReportDocument
+        reportdocument = New CrystalReport1
+        reportdocument.SetDataSource(table)
+        Form3.CrystalReportViewer1.ReportSource = reportdocument
+        Form3.ShowDialog()
+        Form3.Dispose()
+    End Sub
+   
+
+    Private Sub Panel2_Paint(sender As System.Object, e As System.Windows.Forms.PaintEventArgs) Handles Panel2.Paint
+
+    End Sub
+End Class
